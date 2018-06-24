3c0b88914fa787d5e55108b6544fcecc4fcc3abc
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-06-23 23:15:58 -0700

    Fix storage bug. Current version meets README spec

--- lib/parse.rb
+++ lib/parse.rb
@@ -18,12 +18,13 @@ class Parser
 
   def incremental_storage
     puts "---Appending the links of 20 .md files to links.yml"
+    add_links_from_md
     store_links_in_file
-    self.md_count = 0
+    @md_count = 0
   end
 
   def get_md_links(append_url)
-    incremental_storage if md_count >= 20
+    incremental_storage if @md_count >= 20
 
     nav_elements = get_nav_elements(append_url)
     nav_elements.each do |el|
@@ -32,8 +33,8 @@ class Parser
       if link.include?("blob")
         if title[-3..-1] == ".md"
           puts "Identified .md file at #{link}"
-          links[link] = []
-          self.md_count += 1
+          @links[link] = []
+          @md_count += 1
         end
       elsif link.include?("tree")
         get_md_links(link)
@@ -43,13 +44,13 @@ class Parser
 
   def store_links_in_file
     name = filename
-    yaml_links = links.to_yaml
+    yaml_links = @links.to_yaml
     if File.file?(name)
       File.prepend_write(name, yaml_links)
     else
       File.write(name, yaml_links)
     end
-    self.links = {}
+    @links = {}
   end
 
   def find_anchors(node)
@@ -57,17 +58,17 @@ class Parser
     node.children.each do |child_node|
       anchors << child_node["href"] if child_node["href"] != nil
       child_anchors = find_anchors(child_node)
-      anchors.concat(child_anchors)
+      anchors.concat(child_anchors) if !child_anchors.empty?
     end
     anchors
   end
 
   def add_links_from_md
-    links.each_key do |md_append_url|
+    @links.each_key do |md_append_url|
       page = get_page(md_append_url)
       sub_page = page.css("article").select {|link| link['class'] == "markdown-body entry-content"}.first
       anchors = find_anchors(sub_page)
-      links[md_append_url] << anchors
+      @links[md_append_url] << anchors
     end
   end
 
@@ -99,5 +100,5 @@ class Parser
   private
 
   attr_reader :agent, :append_url, :filename
-  attr_accessor :links, :md_count
+  attr_accessor
 end
